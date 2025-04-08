ac274cdab61c1168268ba1a637a45ce3fe34c88e
Arthuranjo <arthurdosanjosnascimento@gmail.com>
2025-04-08 19:52:06 -0300

    voltando

--- main.py
+++
@@ -1,8 +0,0 @@
-from database.conexao import conectar
-
-try:
-    db = conectar()
-    print("Conexão realizada com sucesso!")
-    db.close()
-except Exception as e:
-    print(f"Erro ao conectar: {e}")
